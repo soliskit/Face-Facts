410c8a9fef96ad4865bd453925a351535a495824
David Solis <doze.inept.0n@icloud.com>
2024-01-22 10:06:28 -0800

    Initial Commit

---
+++ Face Facts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Face Facts
+//
+//  Created by David Solis on 1/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Face Facts/Face_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_FactsApp.swift
+//  Face Facts
+//
+//  Created by David Solis on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Face_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
