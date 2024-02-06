5c47c2a2af0e24f509e026b669d73121f1649de5
David Solis <doze.inept.0n@icloud.com>
2024-02-05 16:34:04 -0800

    access swiftData store using modelContext from enviroment

--- Face Facts/ContentView.swift
+++ Face Facts/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
     @State private var path = [Person]()
     @Query var people: [Person]
     
